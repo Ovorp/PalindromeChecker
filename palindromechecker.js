5a4993126aa9aa989814f3a6fe94652fad6f4bb2
Nathaniel <57385976+Ovorp@users.noreply.github.com>
2021-02-13 17:02:41 -0700

    Update palindromechecker.js
    added js code checker

--- palindromechecker.js
+++ palindromechecker.js
@@ -1,1 +1,23 @@
-//this is the JS for the Palindrome Checker
+function palindrome(str) {
+  let regex = /[a-z0-9]/g;
+let newStr = str.toLowerCase().match(regex);
+let revStr = [];
+newStr.map(x => revStr.unshift(x))
+let answer = [];
+console.log(newStr)
+
+for (let i=0; i < newStr.length; i++) {
+
+  if(newStr[i] == revStr[i]) {
+    answer.push(true);
+  } 
+  else {answer.push(false)}
+}
+return answer.every(x => x==true)
+
+}
+
+
+
+let a = palindrome("A man, a plan, a canal. Panama");
+console.log(a)
